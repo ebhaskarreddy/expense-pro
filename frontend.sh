fbbf7f422a37fe7ffd66b7071e9521301642ffc7
enjamoorbhaskar <ebhaskarreddy967@gmail.com>
2023-11-02 10:53:42 +0530

    comments

--- frontend.sh
+++ frontend.sh
@@ -2,20 +2,20 @@ echo -e "\e[32m Installing Nginx \e[om"
 
 dnf install nginx -y
 
-echo -e "\e[36m Copeing Expense Confi \e[om"
+echo -e "\e[31m Copyinng Expense Confi \e[om"
 cp expense.conf /etc/nginx/default.d/expense.conf
 
-echo -e "\e[36m Cleaning Old Nginx \e[om"
+echo -e "\e[33m Cleaning Old Nginx \e[om"
 rm -rf /usr/share/nginx/html/*
 
-echo -e "\e[36m Download Frontend Aplication Code \e[om"
+echo -e "\e[34m Download Frontend Aplication Code \e[om"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
-echo -e "\e[36m Extracting Downloaded Aplication Conten \e[om"
+echo -e "\e[35m Extracting Downloaded Aplication Conten \e[om"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-echo -e "\e[34m Starting Nginx Service \e[om"
+echo -e "\e[36m Starting Nginx Service \e[om"
 systemctl enable nginx
 systemctl restart nginx
 
