315999bbb97066b8352e09e8343a22702f48c445
enjamoorbhaskar <ebhaskarreddy967@gmail.com>
2023-11-02 12:43:07 +0530

    comments

--- mysql.sh
+++ mysql.sh
@@ -1,16 +1,16 @@
 
 echo -e "\e[36 Disable Old Mysql \e[om"
-dnf module disable mysql -y
+dnf module disable mysql -y &>>/tmp/expense.log
 
 echo -e "\e[36m Copying Mysql Repo File \e[om"
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/expense.log
 
 echo -e "\e[36m Install Mysql Server \e[om"
-dnf install mysql-community-server -y
+dnf install mysql-community-server -y &>>/tmp/expense.log
 
 echo -e "\e[36m Start Mysql Server \e[om"
-systemctl enable mysqld
-systemctl start mysqld
+systemctl enable mysqld  &>>/tmp/expense.log
+systemctl start mysqld &>>/tmp/expense.log
 
 echo -e "\e[36m Mysql Security With Password \e[om"
-mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>/tmp/expense.log
