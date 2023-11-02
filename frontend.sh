8106a956a1b7b8d3bf45742d9c8078b68ffe2f45
enjamoorbhaskar <ebhaskarreddy967@gmail.com>
2023-11-02 14:29:25 +0530

    comments

--- frontend.sh
+++ frontend.sh
@@ -1,23 +1,26 @@
-echo -e "\e[32m Installing Nginx \e[om"
+log_file=/tmp/expense.log
+color="\e[31m"
 
-dnf install nginx -y
+echo -e "${color} Installing Nginx \e[om"
+
+dnf install nginx -y &>>log_file
 
 echo -e "\e[31m Copyinng Expense Confi \e[om"
-cp expense.conf /etc/nginx/default.d/expense.conf
+cp expense.conf /etc/nginx/default.d/expense.conf &>>log_file
 
 echo -e "\e[33m Cleaning Old Nginx \e[om"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>log_file
 
 echo -e "\e[34m Download Frontend Aplication Code \e[om"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>log_file
 
 echo -e "\e[35m Extracting Downloaded Aplication Conten \e[om"
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html &>>log_file
+unzip /tmp/frontend.zip &>>log_file
 
 echo -e "\e[36m Starting Nginx Service \e[om"
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx &>>log_file
+systemctl restart nginx &>>log_file
 
 
 
