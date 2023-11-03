073cd44a8e5ed968e1023aa10141395497d2ee74
enjamoorbhaskar <ebhaskarreddy967@gmail.com>
2023-11-03 17:02:57 +0530

    ifcondition

--- backend.sh
+++ backend.sh
@@ -3,52 +3,100 @@ color="\e[36m"
 
 echo -e "${color} Disable Old Nodejs \e[om"
 dnf module disable nodejs -y &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[33m FAILURE \e[0m"
+    fi
 
 echo -e "${color} Enable Nodejs Version 18 \e[om"
 dnf module enable nodejs:18 -y &>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[33m FAILURE \e[0m"
+    fi
 
 echo -e "${color} Installing Nodejs \e[om"
 dnf install nodejs -y &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[33m FAILURE \e[0m"
+    fi
 
 echo -e "${color} Copying Backend Service \e[om"
 cp backend.service /etc/systemd/system/backend.service &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[33m FAILURE \e[0m"
+    fi
 
-echo -e "${color} Add User \e[om"
+echo -e "${color} Addapplication User \e[om"
 useradd expense &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[33m FAILURE \e[0m"
+    fi
 
-echo -e "${color} Creating Directory \e[om"
+echo -e "${color} Creating Appliction Directory \e[om"
 mkdir /app &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[33m FAILURE \e[0m"
+    fi
 
-echo -e "${color} Delete Old Content \e[om"
+echo -e "${color} Delete Old Application Content \e[om"
 rm -rf /app* &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[33m FAILURE \e[0m"
+    fi
 
 echo -e "${color} Downloading Conten File \e[om"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[33m FAILURE \e[0m"
+    fi
 
 echo -e "${color} Extracting Downloaded Content \e[om"
 unzip /tmp/backend.zip &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[33m FAILURE \e[0m"
+    fi
 
 echo -e "${color} Install Nodejs Dependency File \e[om"
 npm install
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[33m FAILURE \e[0m"
+    fi
 
 echo -e "${color} Install Mysql Schema \e[om"
 dnf install mysql -y &>>log_file
 mysql -h mysql-dev.rdevops650nline.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[33m FAILURE \e[0m"
+    fi
 
 echo -e "${color} Start Backend Server \e[om"
 systemctl daemon-reload
 systemctl enable backend
 systemctl restart backend &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[33m FAILURE \e[0m"
+    fi
 
--- mysql.sh
+++ mysql.sh
@@ -3,23 +3,47 @@ color="\e[36m"
 
 echo -e "${color} Disable Old Mysql \e[om"
 dnf module disable mysql -y &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[33m FAILURE \e[0m"
+    fi
 
 echo -e "${color} Copying Mysql Repo File \e[om"
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[33m FAILURE \e[0m"
+    fi
 
 echo -e "${color} Install Mysql Server \e[om"
 dnf install mysql-community-server -y &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[33m FAILURE \e[0m"
+    fi
 
 echo -e "${color} Start Mysql Server \e[om"
 systemctl enable mysqld  &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[33m FAILURE \e[0m"
+    fi
 
 systemctl start mysqld &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[33m FAILURE \e[0m"
+    fi
 
 echo -e "${color} Mysql Security With Password \e[om"
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[33m FAILURE \e[0m"
+    fi
