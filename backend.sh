993dea1d1f38dc1047a8e8b71357a6f199e97a26
enjamoorbhaskar <ebhaskarreddy967@gmail.com>
2023-11-03 22:13:37 +0530

    function

--- backend.sh
+++ backend.sh
@@ -1,6 +1,15 @@
 log_file=/tmp/expense.log
 color="\e[36m"
 
+status_check(){
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e[0m"
+    else
+      echo -e "\e[33m FAILURE \e[0m"
+      fi
+
+}
+
 echo -e "${color} Disable Old Nodejs \e[om"
 dnf module disable nodejs -y &>>log_file
 if [ $? -eq 0 ]; then
