5a997d6dfecc4629a38bec793b876471a735e874
enjamoorbhaskar <ebhaskarreddy967@gmail.com>
2023-11-04 11:02:53 +0530

    function

--- backend.sh
+++ backend.sh
@@ -49,7 +49,6 @@ curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip 
 status_check
 
 echo -e "${color} Extracting Downloaded Content \e[om"
-cd /app
 unzip /tmp/backend.zip &>>log_file
 status_check
 
